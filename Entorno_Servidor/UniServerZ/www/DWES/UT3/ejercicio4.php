da15335feb6220c98630ace91ad7ff77359c603d
delatorresmr1m <delatorresmr1m@gmail.com>
2026-09-23 13:17:24 +0200

    .

--- Entorno_Servidor/UniServerZ/www/DWES/UT3/ejercicio4.php
+++ Entorno_Servidor/UniServerZ/www/DWES/UT3/ejercicio4.php
@@ -1,40 +1,39 @@
 <?php
 
 /**********************************************************************
-4.-Simula una calculadora básica(sumar, restar, dividir y multiplicar). 
-Inicializa dos variables numéricas a un valor y una variable string a un 
-carácter (+,-,/,x).
-Visualiza el resultado.
+    4.-Simula una calculadora básica(sumar, restar, dividir y multiplicar).
+    Inicializa dos variables numéricas a un valor y una variable string a un
+    carácter (+,-,/,x).
+    Visualiza el resultado.
 **********************************************************************/
 
 $valor1 = 2;
 $valor2 = 3;
-$operando = "+";
+$operando = "x";
+
+$mensaje = "La variable \"\$valor1\" tiene un valor de $valor1 y la variable \"\$valor2\" tiene un valor de $valor2<br>";
 
 switch ($operando) {
     case "+":
-        echo "La variable \"\$valor1\" tiene un valor de $valor1 y la variable \"\$valor2\" tiene un valor de $valor2<br>";
-        echo "El resultado de la suma es: " . ($valor1 + $valor2);
+        $mensaje .= "El resultado de la suma es: " . ($valor1 + $valor2);
         break;
     case "-":
-        echo "La variable \"\$valor1\" tiene un valor de $valor1 y la variable \"\$valor2\" tiene un valor de $valor2<br>";
-        echo "El resultado de la resta es: " . ($valor1 - $valor2);
+        $mensaje .= "El resultado de la resta es: " . ($valor1 - $valor2);
         break;
-    case "*":
-        echo "La variable \"\$valor1\" tiene un valor de $valor1 y la variable \"\$valor2\" tiene un valor de $valor2<br>";
-        echo "El resultado de la multiplicación es: " . ($valor1 * $valor2);
+    case "x":
+        $mensaje .= "El resultado de la multiplicación es: " . ($valor1 * $valor2);
         break;
     case "/":
-        echo "La variable \"\$valor1\" tiene un valor de $valor1 y la variable \"\$valor2\" tiene un valor de $valor2<br>";
         if ($valor2 != 0) {
-            echo "El resultado de la división es: " . ($valor1 / $valor2);
+            $mensaje .= "El resultado de la división es: " . ($valor1 / $valor2);
         } else {
-            echo "Error: División por cero";
+            $mensaje .= "Error: División por cero";
         }
         break;
     default:
-        echo "Operando no válido";
+        $mensaje = "Operando no válido";
 }
 
+echo $mensaje;
 
 ?>
